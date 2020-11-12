9d6d5e3d5fdb992b16ba35a6676eeb5eb44050b5
Abhishek Raj <abhishekraj272@gmail.com>
2020-11-12 15:14:29 +0530

    added build

---
+++ build/precache-manifest.f12b9f5a8b08806c88a939e1f8e6905c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcc32ff7b3fbb6b6989838734d2adda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ac3ac909313c4f99d1",
+    "url": "/static/css/2.f8fb7830.chunk.css"
+  },
+  {
+    "revision": "f280bc397dc184d9d701",
+    "url": "/static/css/main.343f3a15.chunk.css"
+  },
+  {
+    "revision": "89ac3ac909313c4f99d1",
+    "url": "/static/js/2.6da826e5.chunk.js"
+  },
+  {
+    "revision": "24f989c6644a3b89a0109f68fca5e058",
+    "url": "/static/js/2.6da826e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f280bc397dc184d9d701",
+    "url": "/static/js/main.9f6381bc.chunk.js"
+  },
+  {
+    "revision": "85f7361cb104bc25c256",
+    "url": "/static/js/runtime-main.ec065ef3.js"
+  },
+  {
+    "revision": "644de09a6da7e61feffa7edb29148b29",
+    "url": "/static/media/logo.644de09a.png"
+  }
+]);
