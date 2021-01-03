c7f20fe39106d7a65b5de7e0eff8fa4d4abd888b
Abhishek Raj <abhishekraj272@gmail.com>
2021-01-03 16:56:16 +0530

    new build

---
+++ build/precache-manifest.cbfd809f7721aa78af45108b8ab05a06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b63c8d7f4e1469b2a87798f8c9adee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8205429ef2947f2bbc29",
+    "url": "/static/css/2.f8fb7830.chunk.css"
+  },
+  {
+    "revision": "30db72ea3ed5a864b95f",
+    "url": "/static/css/main.3e5cd5b1.chunk.css"
+  },
+  {
+    "revision": "8205429ef2947f2bbc29",
+    "url": "/static/js/2.7395d37c.chunk.js"
+  },
+  {
+    "revision": "1a8ea2f9c87277e9b2caa0eee12f03e3",
+    "url": "/static/js/2.7395d37c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30db72ea3ed5a864b95f",
+    "url": "/static/js/main.eb3cf35f.chunk.js"
+  },
+  {
+    "revision": "85f7361cb104bc25c256",
+    "url": "/static/js/runtime-main.ec065ef3.js"
+  },
+  {
+    "revision": "644de09a6da7e61feffa7edb29148b29",
+    "url": "/static/media/logo.644de09a.png"
+  }
+]);
